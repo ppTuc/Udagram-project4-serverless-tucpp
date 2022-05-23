0eaf0e90eead91c90161bb77f4ff226f0178e3b9
ppTuc <tucppfx11443@funix.edu.vn>
2022-05-23 22:18:00 +0700

    first-commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'uk2ko8cg5k'
+export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-r35epbqy.us.auth0.com',           // Auth0 domain
+  clientId: 'fi29uHgyhNynGdAi1ShjsUu1RP4DLnvo',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
